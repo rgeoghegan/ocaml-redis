4c58edaeb0f51638f506e6009c9beea79418a34b
Rory Geoghegan <rorygeoghegan@murat.local>
2009-11-28 18:39:08 +0000

    Have all util tests

--- tests/test_redis_util.ml
+++ tests/test_redis_util.ml
@@ -11,3 +11,93 @@ let test_read_string () =
             = "test string"
         )
     end;;
+
+let test_send_text () =
+    let test_func connection =
+        Redis_util.send_text "foo" connection
+    in
+    Script.use_test_script [Script.ReadThisLine("foo")] test_func;;
+
+let test_receive_answer () =
+    let test_func connection =
+        begin
+            assert (
+                Redis_util.receive_answer connection
+                = Redis_util.Status("bar")
+            );
+            assert (
+                Redis_util.receive_answer connection
+                = Redis_util.Undecipherable
+            );
+            assert (
+                Redis_util.receive_answer connection
+                = Redis_util.Integer(42)
+            );
+            assert (
+                Redis_util.receive_answer connection
+                = Redis_util.Bulk(Redis_util.Data("aaa"))
+            );
+            assert (
+                Redis_util.receive_answer connection
+                = Redis_util.Bulk(Redis_util.Nil)
+            );
+            assert (
+                Redis_util.receive_answer connection
+                = Redis_util.Multibulk([Redis_util.Data("rory"); Redis_util.Data("tim")])
+            )
+        end
+    in
+    Script.use_test_script 
+        [
+            Script.WriteThisLine("+bar"); (* Status *)
+            Script.WriteThisLine("!"); (* Undecipherable *)
+            Script.WriteThisLine(":42"); (* Integer *)
+            Script.WriteThisLine("$3"); (* Bulk *)
+            Script.WriteThisLine("aaa");
+            Script.WriteThisLine("$-1"); (* Nil Bulk *)
+            Script.WriteThisLine("*2"); (* Multibulk *)
+            Script.WriteThisLine("$4");
+            Script.WriteThisLine("rory");
+            Script.WriteThisLine("$3");
+            Script.WriteThisLine("tim")
+        ]
+        test_func;;
+
+let test_send_and_receive_command () =
+    let test_func connection =
+        begin
+            assert (
+                Redis_util.send_and_receive_command "foo" connection
+                = Redis_util.Status("bar")
+            );
+            assert (
+                Redis_util.send_and_receive_command "foo" connection
+                = Redis_util.Undecipherable
+            );
+            assert (
+                Redis_util.send_and_receive_command "foo" connection
+                = Redis_util.Integer(42)
+            );
+            assert (
+                Redis_util.send_and_receive_command "foo" connection
+                = Redis_util.Bulk(Redis_util.Data("aaa"))
+            );
+        end
+    in
+    Script.use_test_script
+        [
+            Script.ReadThisLine("foo");
+            Script.WriteThisLine("+bar");
+
+            Script.ReadThisLine("foo");
+            Script.WriteThisLine("!"); (* Undecipherable *)
+
+            Script.ReadThisLine("foo");
+            Script.WriteThisLine(":42"); (* Integer reply *)
+
+            Script.ReadThisLine("foo");
+            Script.WriteThisLine("$3"); (* Bulk reply *)
+            Script.WriteThisLine("aaa")
+        ]
+        test_func;;
+
