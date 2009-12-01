ff3f2a7e6b3ce796f0a22d3ac11b4e2d4a9d26a6
Rory Geoghegan <r.geoghegan@gmail.com>
2009-12-01 21:48:16 +0000

    Added KEYS keyword

---
+++ tests/test_redis_key_space.ml
@@ -0,0 +1,21 @@
+(* Tests for "Commands operating on the key space" *)
+
+let test_keys () =
+    let test_func connection =
+        Redis.set "rory" "cool" connection;
+        Redis.set "tim" "uncool" connection;
+        assert(["rory"; "tim"] = Redis.keys "*" connection)
+    in
+    Script.use_test_script
+        [
+            Script.ReadThisLine("SET rory 4");
+            Script.ReadThisLine("cool");
+            Script.WriteThisLine("+OK");
+            Script.ReadThisLine("SET tim 6");
+            Script.ReadThisLine("uncool");
+            Script.WriteThisLine("+OK");
+            Script.ReadThisLine("KEYS *");
+            Script.WriteThisLine("$8");
+            Script.WriteThisLine("rory tim")
+        ]
+        test_func;;
